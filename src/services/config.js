7235e5a397e8ec074898bb113c7e29000dda1686
Katherine Aguilera <katherine.aguilera@huelladigital.tv>
2021-02-11 09:51:25 -0500

    Actualización de API

--- src/services/config.js
+++ src/services/config.js
@@ -1,7 +1,7 @@
 // alacenamiento parametros de config
 const configService = {
   // url para interactuar con contenido
-  apiUrl: 'https://platzi-music-api.now.sh'
+  apiUrl: 'https://platzi-music-api.herokuapp.com/'
 }
 // exportar objeto
 export default configService
